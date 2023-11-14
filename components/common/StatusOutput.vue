29146d0a4b9803bf9d22f7f0a0ed1847939caa76
Roman <romazelenskiy2000@gmail.com>
2023-11-14 19:30:53 +0200

    fix comit

--- components/common/StatusOutput.vue
+++ components/common/StatusOutput.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
   <p
-    :style="`background: ${color}80; color: ${color}; border-color: ${color};`"
+    :style="`background: ${color}30; color: ${color}; border-color: ${color};`"
     class="px-[8px] inline-block md:px-[10px] py-[4px] md:py-[5px] font-medium text-[9px] md:text-[12px] rounded-[3px] text-center border-black border"
   >
     <slot />
